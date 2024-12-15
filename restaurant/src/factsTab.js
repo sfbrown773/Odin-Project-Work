f3c0e316837183b135c468290997c1ae175a759a
Spencer Brown <sfbrown773@gmail.com>
2024-12-15 21:55:39 +0300

    bandit practice

--- restaurant/src/factsTab.js
+++ restaurant/src/factsTab.js
@@ -1,23 +1,37 @@
+import { createContent } from "./index2";
 function facts() {
     const factsTab = document.querySelector('#facts');
+    function clearDom() {
+        const oldContent = document.querySelector('#content');
+        while (oldContent.firstChild) {
+            oldContent.removeChild(oldContent.firstChild);
+          }
+    }
     
     factsTab.addEventListener('click', () => {
-        function clearDom() {
-            while (document.body.firstChild) {
-                document.body.removeChild(document.body.firstChild);
-              }
+
+        function displayFacts() {
+                const contentDiv = document.querySelector('#content');
+                const newContent = document.createElement('div')
+
+                const para = document.createElement('p')
+                para.textContent = 'you broke the website'
+
+                const backButton = document.createElement('button')
+                backButton.textContent = 'Back to home page'
+                backButton.id = 'backHome'
+            
+                contentDiv.appendChild(newContent);
+                newContent.appendChild(para);
+                contentDiv.appendChild(backButton);
+                
+                backButton.addEventListener('click', () => {
+                    clearDom();
+                    createContent();
+                }); 
         }
         clearDom();
-
-        const contentDiv = document.createElement('div')
-
-        const para = document.createElement('p')
-        para.textContent = 'Some other text'
-     
-        contentDiv.appendChild(para);
-    })
+        displayFacts();       
+    });
 }
-
-
-
 export {facts}
